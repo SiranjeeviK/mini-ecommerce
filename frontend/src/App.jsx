e60bc49a3867687dadde60ad701f3f24bb1c6fc8
Siranjeevi K <81350268+SiranjeeviK@users.noreply.github.com>
2024-09-29 22:15:56 +0530

    feat: Refactored Home.jsx and App.jsx and added React Router Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
